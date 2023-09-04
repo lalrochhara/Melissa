8251feb189dfca85aa6fefbf3aa974f79074c75c
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-09-04 22:04:40 +0530

    Update __init__.py

--- Melissa/__init__.py
+++ Melissa/__init__.py
@@ -144,7 +144,7 @@ DEV_USERS.add(OWNER_ID)
 DEV_USERS.add(6161727895)
 
 
-updater = ApplicationBuilder().token("TOKEN").build()
+updater = tg.ApplicationBuilder().token(TOKEN).build()
 telethn = TelegramClient("Melissa", API_ID, API_HASH)
 
 pbot = Client("Melissa", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
